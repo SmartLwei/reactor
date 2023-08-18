face740b260b2b8691cdf3467e36685c46a7f66c
宇益 <yuyi.lw@antgroup.com>
2023-08-18 16:56:35 +0800

    fixup! feat: optimize the project structure

---
+++ app/test/src/test/java/com/example/test/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import com.example.bootstrap.DemoApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DemoApplication.class)
+class DemoApplicationTests {
+
+    @Test
+    void test() {
+    }
+
+}
